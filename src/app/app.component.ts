244047065dbba0cf5779629c4517daa1715cef7a
sw <rj9101@me-zoo.com>
2021-09-24 16:56:15 +0900

    eslint 적용

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,12 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnChanges{
-  ngOnChanges(changes: SimpleChanges): void {
-    // document.querySelectorAll(".page-link").forEach(item => {
-    //   // if (item.getAttribute("aria-label"))
-    //   console.log(item);
-    // })
-  }
-  
+export class AppComponent {
   title = 'pagination';
   page = 1;
-  
+
   clickFirst = (event: any) => {
     console.log(event.target);
     event.preventDefault();
@@ -24,12 +17,14 @@ export class AppComponent implements OnChanges{
     //   if (item.getAttribute("aria-label"))
     //   console.log();
     // })
-    console.log(1);
-  }
+    console.log('1');
+    console.log('1');
+    console.log('1');
+  };
 
   clickLast = (event: any) => {
     event.preventDefault();
     event.stopPropagation();
     console.log(2);
-  }
+  };
 }
