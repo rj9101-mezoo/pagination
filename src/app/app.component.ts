776a5ab81dc9e51d6efa2e572580797faec4dce3
sw <rj9101@me-zoo.com>
2021-09-24 16:45:39 +0900

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    // document.querySelectorAll(".page-link").forEach(item => {
+    //   // if (item.getAttribute("aria-label"))
+    //   console.log(item);
+    // })
+  }
+  
   title = 'pagination';
+  page = 1;
+  
+  clickFirst = (event: any) => {
+    console.log(event.target);
+    event.preventDefault();
+    event.stopPropagation();
+    // document.querySelectorAll(".page-link").forEach(item => {
+    //   if (item.getAttribute("aria-label"))
+    //   console.log();
+    // })
+    console.log(1);
+  }
+
+  clickLast = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(2);
+  }
 }
